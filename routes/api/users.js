684bc2169335c5f0a23e19793551f9e729d50201
Micah Lund <micah.r.lund@gmail.com>
2021-05-01 17:36:11 -0600

    Added the JSON Web Token logic to the user route.

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
+
 const User = require('../../models/User');
 
 // @route   POST api/users
@@ -48,7 +51,21 @@ router.post('/', [
         user.password = await bcrypt.hash(password, salt);
         await user.save();
 
-        res.send('User registered')
+        const payload = {
+            user: {
+                id: user.id 
+            }
+        }
+
+        jwt.sign(
+            payload, 
+            config.get('jwtToken'), 
+            { expiresIn: 3600 },
+            (err, token) => {
+                if (err) throw err;
+                res.json({ token });
+            }
+        );
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Server error');
