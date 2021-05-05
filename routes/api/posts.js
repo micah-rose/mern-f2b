95a703d8799ca6c79c078c87bfad579686e7eb46
Micah Lund <micah.r.lund@gmail.com>
2021-05-05 16:39:32 -0600

    Failed to change the request verb from GET to POST.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -11,7 +11,7 @@ const Post = require('../../models/Post');
 // @route   POST api/posts
 // @desc    Create a post
 // @access  Private
-router.get('/', [auth, [
+router.post('/', [auth, [
     check('text', 'Text is required').not().isEmpty()
 ]], async(req, res) => {
     const errors = validationResult(req);
@@ -22,12 +22,12 @@ router.get('/', [auth, [
     try {
         const user = await User.findById(req.user.id.select('-password'));
 
-        const newPost = {
+        const newPost = new Post ({
             text: req.body.text,
             name: user.name,
             avatar: user.avatar,
             user: req.user.id
-        }
+        })
 
         const post = await newPost.save();
 
